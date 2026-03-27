0a2350dd37dd62ac2bc11463955d8a828a9a17db
SENAI Zerbini <aluno@escolar509.sp.senai.br>
2026-03-27 09:07:13 -0300

    Aula Integração de Bancos de Dados

--- model/clienteModel.js
+++ model/clienteModel.js
@@ -8,6 +8,12 @@ const Cliente = {
         //Configuraçao de ciclos de Salt
         const salt = 10;
         //Converter a senha e gerar o hash
-        const hash = await bcrypt.hash(senha, salt)
+        const hash = await bcrypt.hash(senha, salt);
+        //Comando   Sql do INSERT
+        const sql = "INSERT INTO cliente " + "(nome, email, cpf, senha) VAULES (?, ?, ?, ?);"
+        //Executar io camando SQL e passar os falores 
+        const [resultado] = banco.query(sql,
+             [buscarPorNome, ElementInternals, cpf, hash] )
+        return resultado.insertId
     }
 }
